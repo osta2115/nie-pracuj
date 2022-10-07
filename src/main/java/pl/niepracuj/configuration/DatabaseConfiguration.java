5bc7679d4a9e46730ff73ac12eaef6c3cde12960
Uzytkownik <uzytkownik@kursGIT.pl>
2022-10-08 00:58:07 +0200

    feature/Zadanie-2-Konfiguracja bazy danych

---
+++ src/main/java/pl/niepracuj/configuration/DatabaseConfiguration.java
@@ -0,0 +1,61 @@
+package pl.niepracuj.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DatabaseConfiguration {
+
+    @Value("${datasource.url}")
+    private String url;
+
+    @Value("${datasource.username}")
+    private String username;
+
+    @Value("${datasource.password}")
+    private String password;
+
+    @Value(("${jpa.properties.hibernate.dialect}"))
+    private String dialect;
+
+    @Value("${jpa.hibernate.ddl-auto}")
+    private String mode;
+
+    @Value("${packges.to.scan}")
+    private String packages;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(url);
+        driverManagerDataSource.setUsername(username);
+        driverManagerDataSource.setPassword(password);
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean getEntityManager() {
+        var localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(getDataSource());
+        localContainerEntityManagerFactoryBean.setPackagesToScan(packages);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        localContainerEntityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", mode);
+        properties.setProperty("hibernate.dialect", dialect);
+        return properties;
+    }
+}
