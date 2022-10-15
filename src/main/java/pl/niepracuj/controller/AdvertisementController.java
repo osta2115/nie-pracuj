196f588dc576d49d40068d1ead910c41c3ce32d4
Uzytkownik <uzytkownik@kursGIT.pl>
2022-10-15 12:49:11 +0200

    feature/Logs added

--- src/main/java/pl/niepracuj/controller/AdvertisementController.java
+++ src/main/java/pl/niepracuj/controller/AdvertisementController.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import pl.niepracuj.exception.exceptions.EntityNotFoundException;
+import pl.niepracuj.model.dto.LogDto;
 import pl.niepracuj.model.dto.advertisement.AdvertisementCreateDto;
 import pl.niepracuj.model.dto.advertisement.AdvertisementDto;
 import pl.niepracuj.model.dto.advertisement.AdvertisementSearchCriteriaDto;
 import pl.niepracuj.service.advertisement.AdvertisementService;
+import pl.niepracuj.service.log.LogService;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,10 +21,13 @@ import java.util.List;
 public class AdvertisementController {
 
     private final AdvertisementService advertisementService;
+    private final LogService logService;
 
     @GetMapping("/all")
-    public List<AdvertisementDto> getAdvertisements() {
-        return advertisementService.getAllAdvertisements();
+    public List<AdvertisementDto> getAdvertisements() throws IOException, InterruptedException {
+        var response = advertisementService.getAllAdvertisements();
+        logService.sendLog(LogDto.getLogDto("All advertisement retrieved"));
+        return response;
     }
 
     @GetMapping("/all/company/{companyId}")
---
+++ src/main/java/pl/niepracuj/service/log/LogServiceImpl.java
@@ -0,0 +1,30 @@
+package pl.niepracuj.service.log;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.niepracuj.model.dto.LogDto;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static pl.niepracuj.util.Utils.toJsonString;
+
+@Service
+@RequiredArgsConstructor
+public class LogServiceImpl implements LogService {
+
+    private final HttpClient httpClient;
+
+    @Override
+    public void sendLog(LogDto logDto) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(toJsonString(logDto)))
+                .uri(URI.create("/logs"))
+                .build();
+
+        httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
